6163f3f4f11a67911b54cf4fc43b6647566439cb
kwon_pc <chris0540@naver.com>
2026-05-02 11:45:21 +0900

    [Feat] 토큰 만료 시간 24시간으로 설정

--- src/main/java/org/example/spring/utils/JwtUtil.java
+++ src/main/java/org/example/spring/utils/JwtUtil.java
@@ -17,7 +17,7 @@ public class JwtUtil {
                 .claim("idx", idx)
                 .claim("email", email)
                 .claim("role", role)
-                .issuedAt(new Date()).expiration(new Date(System.currentTimeMillis() + 300000)).signWith(encodedKey).compact();
+                .issuedAt(new Date()).expiration(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000L)).signWith(encodedKey).compact();
 
         return jwt;
     }
